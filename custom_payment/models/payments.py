4def3ff201f6d39c28911bbc547dd9bf52210945
hassan <hassan>
2022-01-11 20:44:14 +0500

    aa

--- custom_payment/models/payments.py
+++ custom_payment/models/payments.py
@@ -8,7 +8,7 @@ class CustomPayment(models.Model):
     _inherit = "account.payment"
 
     pv_num = fields.Char(string='PV No.')
-    reference_num = fields.Char(string='Ref. Number')
+    reference_num = fields.Char(string='Reference No.')
     narration_text = fields.Text(string='Narration')
     post_date = fields.Date(string="Posting Date", default=datetime.now())
     pv_selection = fields.Selection([
@@ -18,7 +18,15 @@ class CustomPayment(models.Model):
         ('requisition', 'Purchase Requisition'),
         ('schedule', 'Costing Schedule'),
         ], string='PV Type', default='general')
-
+    rv_selection = fields.Selection([
+        ('general', 'General'),
+        ('advance', 'Advance'),
+        ('invoice', 'Sales Invoice'),
+        ('jv', 'Journal Voucher'),
+    ], string='RV Type', default='general')
+    rv_category = fields.Selection([
+        ('pdc', 'PDC'),
+    ], string='RV Category')
     bal_amount = fields.Float(string='Balance Amount')
 
 
