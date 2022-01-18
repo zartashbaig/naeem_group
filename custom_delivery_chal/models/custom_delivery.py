dae646e3cc8f4f84b9903ead732f347d24e7e19c
ahmad225 <ahmadshahid5776@gmail.com>
2022-01-18 20:29:04 +0500

    New Content

--- custom_delivery_chal/models/custom_delivery.py
+++ custom_delivery_chal/models/custom_delivery.py
@@ -28,9 +28,6 @@ class CustomDeliveryChal(models.Model):
     ], string="DC Type")
 
 
-
-
-
 class CustomStockMov(models.Model):
     _inherit = "stock.move"
 
@@ -40,4 +37,46 @@ class CustomStockMov(models.Model):
     balance_inherit = fields.Float(string='Bal qty')
     quantity_inherit = fields.Float(string='Qty')
     bonus_qty_inherit = fields.Float(string='Bonus Qty')
-    total_qty_inherit = fields.Float(string='Total Qty')
+
+class DOExt(models.Model):
+    _inherit = "stock.move.line"
+
+    bonus_quantity = fields.Float(string='Bonus Qty', compute='_gets_data')
+    ordered_quantity = fields.Float(string='Ordered Qty', compute='_gets_data')
+    qty_done = fields.Float('Done', default=0.0, digits='Product Unit of Measure', copy=False)
+    total_qty_inherit = fields.Float(string='Total Qty', compute='_compute_amount_total')
+    product_uom_qty = fields.Float('Reserved', default=0.0, digits='Product Unit of Measure', required=True)
+    brand_id = fields.Many2one('product.brand', string='Brand')
+    wh_id = fields.Many2one('stock.warehouse', string='Warehouse')
+
+
+    @api.model
+    def _gets_data(self):
+        self.bonus_quantity = 0.0
+        self.ordered_quantity = 0.0
+
+        for move in self:
+            # if not move.move_lines and not move.move_line_ids:
+            if not (move.picking_id and move.picking_id.group_id):
+                continue
+            picking = move.picking_id
+            sale_order = self.env['sale.order'].sudo().search([
+                ('procurement_group_id', '=', picking.group_id.id)], limit=1)
+            if sale_order:
+                for line in sale_order.order_line:
+                    if line.product_id.id != move.product_id.id:
+                        continue
+                    move.update({
+                        'bonus_quantity': line.bonus_quantity,
+                        'ordered_quantity': line.product_uom_qty,
+                    })
+            else:
+                move.bonus_quantity = False
+
+    @api.depends('bonus_quantity', 'total_qty_inherit', 'ordered_quantity')
+    def _compute_amount_total(self):
+        self.total_qty_inherit = self.bonus_quantity + self.ordered_quantity
+
+    # @api.depends('bonus_quantity', 'product_uom_qty', 'qty_done')
+    # def _compute_amount_done(self):
+    #     self.qty_done = self.bonus_quantity + self.product_uom_qty
